eedd036e1cb3706118152182c6a1b22fc9382c23
ylmngo <aamerasim45@gmail.com>
2024-10-04 20:26:48 +0530

    added swagger docs

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask 
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from flask_swagger_ui import get_swaggerui_blueprint
 import os 
 
 load_dotenv() 
 app = Flask(__name__) 
 client = MongoClient(os.environ.get("MONGO_URI"))
 db = client.Users 
+SWAGGER_URL = "/swagger" 
+API_URL="/static/swagger.json"
 
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, 
+    API_URL, 
+    config={
+        'app_name': "Corider Assignment"
+    }
+)
--- core/server.py
+++ core/server.py
@@ -1,8 +1,9 @@
-from core import app
+from core import app, swagger_ui_blueprint, SWAGGER_URL
 from flask import jsonify
 from .apis.users import user_resource
 
 app.register_blueprint(user_resource, url_prefix="/users")
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/')
 def ready(): 
